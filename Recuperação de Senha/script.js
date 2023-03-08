e1ae5ae3b497fa6de2b7ec5c5239f1e1432d8897
JP Shawty <105120651+jpshawty38@users.noreply.github.com>
2023-03-08 16:00:12 -0300

    Atualização

---
+++ Recuperação de Senha/script.js
@@ -0,0 +1,27 @@
+
+const email = document.getElementById('email')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+})
+
+function checkInputs() {
+
+    const emailValue = email.value.trim()
+
+    if(emailValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(email, 'Preencha esse campo')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email inválido')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(email)
+    }
+
+    function isEmail(email) {
+        return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+    }}
