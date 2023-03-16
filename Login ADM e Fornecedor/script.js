60a76a6b5a5d7c9ce044935849bdf5a3e22da6c9
Felipe Silva <felipejesusdasilva02@gmail.com>
2023-03-16 17:33:10 -0300

    update verificação login

--- Login ADM e Fornecedor/script.js
+++ Login ADM e Fornecedor/script.js
@@ -1,10 +1,60 @@
-function verifypass(){
-    var senha = document.getElementsByName('senha')[0].value;
-    
+function verificar() {
+    var senha = document.getElementById('senha').value;
+    var feed = document.getElementsByClassName('feed')
+
+
     var maiscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numero = "0123456789";
-    var especiais = "!@#$%&*_";
+    var numeros = "0123456789";
 
-    console.log(senha.indexOf('a'))
 
+    var m = [];
+    var n = [];
+
+
+
+    for (i = 0; i < senha.length; i++) {
+        m.push(maiscula.indexOf(senha.charAt(i)))
+        var maxM = Math.max.apply(null, m)
+        if (maxM >= 0) {
+            feed[1].style.color = "green";
+        } else {
+            feed[1].style.color = "red";
+        }
+
+        n.push(numeros.indexOf(senha.charAt(i)))
+        var maxN = Math.max.apply(null, n)
+        if (maxN >= 0) {
+            feed[2].style.color = "green";
+        } else {
+            feed[2].style.color = "red";
+        }
+
+        if (senha.length >= 8) {
+            feed[0].style.color = "green";
+        } else {
+            feed[0].style.color = "red";
+        }
+    }
 }
+
+function entrar(){
+    window.location.href = "https://www.google.com.br";
+}
+
+var text = document.getElementById('text')
+var botão = document.getElementById('botão')
+
+text.addEventListener("input" , function() {
+    if(text.value !== "" ){
+        var minhaF = function verificar(){}
+        minhaF() 
+        botão.disabled = false        
+    }else{
+        
+        botão.disabled = true
+        
+    }
+}
+
+
+)
