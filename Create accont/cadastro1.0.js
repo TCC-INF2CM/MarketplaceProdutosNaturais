24153f35876bd22138d063d357ee7d7194cf261b
Felipe Silva <felipejesusdasilva02@gmail.com>
2023-03-26 18:16:34 -0300

    updated

--- Create accont/cadastro1.0.js
+++ Create accont/cadastro1.0.js
@@ -1,8 +1,16 @@
+<<<<<<< Updated upstream
 const label = document.getElementById('label')
 const name = document.getElementById('name')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const password2 = document.getElementById('password2')
+=======
+const form = document.getElementById('form')
+const username = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordtwo = document.getElementById('password2')
+>>>>>>> Stashed changes
 
 label.addEventListener('enviar', (e) => {
     e.preventDefault()
