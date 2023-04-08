f739cc02d1516c56f454329f380659e2138f9644
Diego Augusto <80727709+Diego0668@users.noreply.github.com>
2023-04-08 14:34:37 -0300

    update

--- Login ADM e Fornecedor/script.js
+++ Login ADM e Fornecedor/script.js
@@ -10,9 +10,14 @@ function verificar() {
     var m = [];
     var n = [];
 
-    
+
 
     for (i = 0; i < senha.length; i++) {
+        if (senha.length <= 8 || senha.value == "") {
+            feed[0].style.color = "red";
+        } else {
+            feed[0].style.color = "green";
+        }
         m.push(maiscula.indexOf(senha.charAt(i)))
         var maxM = Math.max.apply(null, m)
         if (maxM >= 0) {
@@ -25,66 +30,55 @@ function verificar() {
         var maxN = Math.max.apply(null, n)
         if (maxN >= 0) {
             feed[2].style.color = "green";
-        } else{
+        } else {
             feed[2].style.color = "red";
         }
-
-        if (senha.length <= 8 || senha.value == "") {
-            feed[0].style.color = "red";
-        } else {
-            feed[0].style.color = "green";
-        }
-        
-        if(feed = ""){
-            feed.style.color = "red"
-        }else{
-            feed.style.color = "green"
-        }
-
     }
 }
 
-function entrar(){
+function entrar() {
     window.location.href = "https://www.google.com.br";
 }
 
-var text = document.getElementById('text')
+var senha = document.getElementById('text')
 var botão = document.getElementById('botão')
 
-text.addEventListener("input" , function() {
-    if(text.value !== "" ){
-        var minhaF = function verificar(){}
-        minhaF() 
-        botão.disabled = false        
-    }else{
+var senha2 = ["000" , "111"]
+
+senha.addEventListener("input", function () {
+    if (senha2.indexOf(senha) > -1) {
+        var minhaF = function verificar() {minhaF()}
         
+        botão.disabled = false
+    } else {
+
         botão.disabled = true
-        
+
     }
 }
 
 )
 
-function esconder(){
+function esconder() {
     var input = document.getElementById(`senha`)
     var minhaDiv = document.getElementById(`minhaDiv`)
     var cond = true
-    if(cond){
-        minhaDiv.style.display = "block" 
+    if (cond) {
+        minhaDiv.style.display = "block"
     }
-    else{
+    else {
         minhaDiv.style.display = "none";
     }
-    
+
 
 }
 const input = document.getElementById("senha");
 const div = document.getElementById("minhaDiv");
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
     // Se o clique não foi no input nem na div, fecha a div
     if (event.target !== input && event.target !== div) {
-      div.style.display = "none";
-    }else{
-      div.style.display = "block";
+        div.style.display = "none";
+    } else {
+        div.style.display = "block";
     }
-  });
+});
