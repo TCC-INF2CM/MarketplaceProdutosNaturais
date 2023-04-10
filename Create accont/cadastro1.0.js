7bf48bb6779337a253bb38e15715b7b8a88e1c75
Felipe Silva <felipejesusdasilva02@gmail.com>
2023-04-10 18:34:07 -0300

    update

--- Create accont/cadastro1.0.js
+++ Create accont/cadastro1.0.js
@@ -1,13 +1,5 @@
 const form = document.getElementById('form')
-const uname = document.getElementById('uname')
-const cnpj = document.getElementById('CNPJ')
-const tel = document.getElementById('tel')
-const cep = document.getElementById('cep')
-const city = document.getElementById('city')
-const email = document.getElementById('email')
-const email2 = document.getElementById('email2')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
+const input = querySelectorAll('.inf')
 
 form.addEventListener('enviar', (e)  => {
     e.preventDefault()
