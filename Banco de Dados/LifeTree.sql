b306f2dd1b5fcdb2d578a26074b145ef2fb24076
Felipe Silva <97049270+FelipeMarani@users.noreply.github.com>
2023-04-09 22:06:11 -0300

    Update LifeTree.sql

--- Banco de Dados/LifeTree.sql
+++ Banco de Dados/LifeTree.sql
@@ -17,16 +17,7 @@ create table Cliente
 	Tel char(13)not null,
 	Email varchar(150)not null,
 	perfil varbinary(max)null,
-	senha varchar(250) not null,
 	statusCliente varchar(20)not null,
-	primary key(id)
-)
-
-create table ADM(
-	id int identity,
-	Nome varchar(150)not null,
-	email varchar(250)not null,
-	senha varchar(150)not null,
 	primary key(id)
 )
 
@@ -55,7 +46,7 @@ create table Fornecedor
 	Informacao varchar(250)null,
 	CEP char(8)not null,
 	City varchar(50)not null,
-	senha varchar(150)not null,
+	Bairro varchar(100)not null,
 	statusFornecedor varchar(20)not null,
 	primary key(id)
 )
@@ -66,45 +57,45 @@ create table Entregador
 	Perfil varbinary(MAX)not null,
 	CNH varchar(11)not null,
 	Tp_Veiculo Varchar(70)not null,
-	senha varchar(150)not null,
 	statusEntregador varchar(20),
 	primary key(id)
 )
 
 create table tpProduto
 (	id int identity,
-	TipodeProduto varchar(150)not null,
+	TpProduto varchar(150)not null,
 	primary key(id)
 )
 
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Chá e Infusões');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('A Granel');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Vitaminas e Suplementos');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Mercearia e Empório');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Bem estar e Beleza');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Refrigferados');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Promoções');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Sem Açucar');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Sem Glútem');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Sem Lactose');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Orgânico');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Vegano');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Vegetariano');
-insert into tpProduto(TipodeProduto)
+insert into tpProduto(TpProduto)
 values('Baixo carboidrato');
+
 
 create table mcProduto
 (	id int identity,
@@ -117,8 +108,7 @@ create table Produto
 	Nome varchar(100)not null,
 	Preco decimal(10,2)not null,
 	layout varchar(MAX),
-	Marca varchar(100)not null,
-	Quantidade int,
+	Disponibilidade int,
 	Fornecedor_id int not null,
 	tpProduto_id int not null,
 	mcProduto_id int not null,
@@ -134,8 +124,7 @@ create table Produto
 
 create table Imagem(
 	id int identity,
-	Nome_arquivo varchar(100)not null,
-	caminho varchar(max)not null,
+	Img varchar(max)not null,
 	statusImage varchar(20)not null,
 	produto_id int not null,
 	primary key(id),
@@ -146,6 +135,7 @@ create table Imagem(
 create table Form_Pagamento
 (	id int identity,
 	Fm_pagamento varchar(50)not null,
+	StatusPg varchar(20)null,
 	primary key(id)
 )
 insert Form_Pagamento(Fm_pagamento)
@@ -160,7 +150,6 @@ values('Boleto 3x')
 create table Venda
 (	id int identity,
 	TT_compra decimal(10,2)not null,
-	statusPagamento varchar(20)not null,
 	Pagamento_id int not null,
 	Cliente_id int not null,
 	Produto_id int not null,
