be81212bb0aad3a21200f7022120f119112ac54a
Diego0668 <diegolimaxd1@gmail.com>
2023-07-12 23:55:54 -0300

    update

--- TELAS SITE/Tela Home/script.js
+++ TELAS SITE/Tela Home/script.js
@@ -4,3 +4,17 @@ myCarousel.addEventListener('slide.bs.carousel', event => {
     interval: 3,
     circle
 })
+
+var contador = 0;
+
+function AddProducts(){
+  contador ++;
+  document.getElementById('contador').innerHTML = contador;
+}
+
+function RemoveProducts(){
+  if(contador > 0){
+    contador --;
+  }
+  document.getElementById('contador').innerHTML = contador;
+}
