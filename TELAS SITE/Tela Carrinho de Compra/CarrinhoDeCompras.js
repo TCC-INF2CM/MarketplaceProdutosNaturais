33b95ca899ab9137a71876a25dbd74bf8e677208
Gustavo Rocha <gurocha05@gmail.com>
2023-07-18 15:03:22 -0300

    MoreThings

--- TELAS SITE/Tela Carrinho de Compra/CarrinhoDeCompras.js
+++ TELAS SITE/Tela Carrinho de Compra/CarrinhoDeCompras.js
@@ -1,16 +1,46 @@
-const removeProductButtons = document.getElementsByClassName("Remove")
-for (var i = 0; i < removeProductButtons.length; i++) {
-    removeProductButtons[i].addEventListener("click", function(event) {
-        event.target.parentElement.parentElement.parentElement.remove()
-    })
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready)
+} else{
+    ready()
+}
+
+var totalAmount = "0,00"
+
+function ready(){
+    const removeProductButtons = document.getElementsByClassName("Remove")
+    for (var i = 0; i < removeProductButtons.length; i++) {
+        removeProductButtons[i].addEventListener("click", removeProduct)
+    }
+
+    const quantityInputs = document.getElementsByClassName("QtdProdNumb")
+    for (var i =0; quantityInputs.length; i++){
+        quantityInputs[i].addEventListener("change", updateTotal)
+ 
+    }
+
+    
 }
 
 
 
+function removeProduct(event){
+    event.target.parentElement.parentElement.parentElement.parentElement.remove()
+    updateTotal()
+}
+
+function updateTotal() {
+totalAmount = 0
 const cartProduct = document.getElementsByClassName("Car_Prod")
 for (var i = 0; i < cartProduct.length; i++) {
-    console.log(cartProduct[i])
-    const productPrice = cartProduct[i].getElementsByClassName("Real_Preco")[0].innerText
-    const productQuantity = cartProduct[i].getElementsByClassName("Prod_Qtd")[0].value
+    //console.log(cartProduct[i])
+    const productPrice = cartProduct[i].getElementsByClassName("Real_Preco")[0].innerText.replace("R$", "").replace(",",".")
+    console.log(productPrice)
+    const productQuantity = cartProduct[i].getElementsByClassName("QtdProdNumb")[0].value
     console.log(productQuantity)
+
+    totalAmount += + productPrice * productQuantity
+}
+totalAmount = totalAmount.toFixed(2)
+totalAmount = totalAmount.replace(".",",")
+document.querySelector(".Total_Price h2").innerText = "R$" + totalAmount
 }
