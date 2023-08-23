7bb0e11b282ed66bde91b40a5b4d3db4b7cbcbfa
JP Shawty <105120651+jpshawty38@users.noreply.github.com>
2023-08-23 15:29:58 -0300

    update

---
+++ pizzaria-da-nonna06/pizzas.js
@@ -0,0 +1,58 @@
+let produtoJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/produto.png',
+        price: [20.00, 23.00, 25.00],
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/produto2.png',
+        price: [21.00, 24.00, 26.00],
+        description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/produto3.png',
+        price: [23.00, 26.00, 28.00],
+        description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+      },
+      {
+        id: 4,
+        name: 'Brasileira',
+        img: 'images/produto4.png',
+        price: [25.00, 28.00, 30.00],
+        description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+      },
+      {
+        id: 5,
+        name: 'Portuguesa',
+        img: 'images/produto5.png',
+        price: [24.00, 27.00, 29.00],
+        description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+      },
+      {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/produto6.png',
+        price: [30.00, 33.00, 35.00],
+        description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+      },
+      {
+        id: 7,
+        name: 'Banana com canela',
+        img: 'images/produto7.png',
+        price: [27.00, 30.00, 32.00],
+        description: 'Mussarela, banana, canela e açúcar'
+      },
+      {
+        id: 8,
+        name: 'Chocolate com morango',
+        img: 'images/produto8.png',
+        price: [30.00, 32.00, 35.00],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      }
+];
