6f12fc95619dc5c4dad979ae3c3b747db17d5ce1
Diego Augusto <80727709+Diego0668@users.noreply.github.com>
2023-08-25 12:09:22 -0300

    Update pizzas.js

--- Carrinho + Home (TESTE)/pizzas.js
+++ Carrinho + Home (TESTE)/pizzas.js
@@ -7,5 +7,6 @@ let pizzaJson = [
         
         description: 'Molho de tomate, camada dupla de mussarela e orégano'
     },
+    informatizar uma loja de produtos naturais e ate entao todos os produtos feitos por essa loja era feito de uma maneira fraca e o nosso sistema foi feito para economizar tempo e agilizar o tempo e o software irá controlar o esquema de estoque 
     
 ];
